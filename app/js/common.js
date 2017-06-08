7980bc609964ff7422f5bcdad1439ab992f8c417
Vitaliy Kozlovskiy <vitaliyk1990@gmail.com>
2017-06-09 01:17:38 +0300

    before build

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,26 @@
 $('#scroll').click(function(event){
-  $('html,body').animate({
+    $('html,body').animate({
         scrollTop: $(".srp").offset().top},
         'slow');
+});
+
+$(".phone").magnificPopup();
+
+$('input').focus(function(){
+   $(this).data('placeholder',$(this).attr('placeholder'))
+   $(this).attr('placeholder','');
+ });
+ $('input').blur(function(){
+   $(this).attr('placeholder',$(this).data('placeholder'));
+ });
+
+// $('.nav__item-active').click(function() {
+//    $('.sub__menu').toggle();
+//    $('.nav__item-active').css('background', 'rgba(0, 0, 0, .3)');
+// });
+
+$('.nav__item-active').click(function(e) {
+    e.preventDefault();            
+    $(this).toggleClass('bgc');
+    $('.sub__menu').toggle();
 });
